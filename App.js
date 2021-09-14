a5e8d780482719a2a784d8591e07614a68ef0c2f
Anurag021 <anu.rawat@outlook.com>
2021-09-14 14:14:52 +0530

    Updated:
    Added Output box to code

--- App.js
+++ App.js
@@ -2,21 +2,26 @@
 // var UserName = prompt("Enter your name");
 // alert ("the name of the user is - "+ UserName) 
 
-
-// var btnTranslate = document.querySelector("#tanslate-Button");
-
-// btnTranslate.addEventListener("click", function clickHandler(){
-//     console.log("clicked!");
-
-// })
+//Query-Selector
 var txtInput = document.querySelector("#txt-area")
 var btnTranslate = document.querySelector("#btn-translate");
-// query selector is used to connect button reference with JS file
+var outputBox = document.querySelector("#output")
  
 //how to intract with the app is by using the event listner, they are click  scroll
-// 
+// Event Listner for Click
 
-btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("Clicked")
+// var inner = outputBox.innerText;
+// console.log(inner)
+
+function clickHandler(){
+
+    console.log("Clicked");
     console.log("input:", txtInput.value);
-})
+    outputBox.innerText = "Translated Text is "+ txtInput.value;
+}
+
+btnTranslate.addEventListener("click",clickHandler)
+
+
+
+ 
