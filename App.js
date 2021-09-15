18244683a41beea44d22f2bc97039403101c121b
Anurag021 <anu.rawat@outlook.com>
2021-09-15 12:48:45 +0530

    Updated App.js
    completed functionality part of the process

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
-
-// var UserName = prompt("Enter your name");
-// alert ("the name of the user is - "+ UserName) 
-
 //Query-Selector
 var txtInput = document.querySelector("#txt-area")
 var btnTranslate = document.querySelector("#btn-translate");
-var outputBox = document.querySelector("#output")
- 
+var outputBox= document.querySelector("#output");
+
+
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var url ="https://api.funtranslations.com/translate/minion.json"
+
+function createURL(text){
+    return url + "?text="+ text;
+}
+
+// another short way of making a server request and get the response processin part
+function doFetch(text){
+    fetch(createURL(text))
+    .then (response => response.json())
+    .then (json=> {
+        var translatedText = json.contents.text  //fetching response from json
+        outputBox.innerText = translatedText   // showing the response in output box -- output
+       
+    }).catch(errorHandler)
+}
+//error handler
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something wrong with server try again after sometime")
+}
+
 //how to intract with the app is by using the event listner, they are click  scroll
 // Event Listner for Click
-
-// var inner = outputBox.innerText;
-// console.log(inner)
+btnTranslate.addEventListener("click",clickHandler)
 
 function clickHandler(){
 
     console.log("Clicked");
     console.log("input:", txtInput.value);
-    outputBox.innerText = "Translated Text is "+ txtInput.value;
-}
 
-btnTranslate.addEventListener("click",clickHandler)
+    doFetch(txtInput.value);
+}
+//fetch response from serer and get the json response and return the text from json response
+// function doFetch(text){
+//     fetch(createURL(text))
+//     .then(function responseHeader(response) {
+//         return response.json()} )
+//     .then(function logJSON(json){
+//         console.log (json.contents.text)
+//         return json.contents.text;
+//     })
+// }
